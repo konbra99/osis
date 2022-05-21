f6912eb1888ff4ed48d650d7eec1a268d92b2430
konbra99 <250339@student.pwr.edu.pl>
2022-05-21 13:47:32 +0200

    poprawka

--- createTests.py
+++ createTests.py
@@ -3,13 +3,12 @@ import numpy as np
 
 
 def generateRandomMachines(number_of_jobs, pmin, pmax):
-    number_of_machines = np.random.randint(
-        pmin, pmax, (number_of_machines, np.random.randint(5, 50)))
-    with open('gereneratedMachinesFor' + str(number_of_jobs) + '.txt', 'w+') as file:
+    number_of_machines = np.random.randint(pmin, pmax, (number_of_jobs, np.random.randint(5, 50)))
+    with open('gereneratedMachinesFor' + str(number_of_jobs) + '.txt', 'w+') as file: 
         file.write(number_of_jobs + " " + number_of_machines)
         for jobs in number_of_machines:
             file.write('\n')
-            for job in jobs:
+            for job in jobs: 
                 file.write(job)
 
 
