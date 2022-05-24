36828c331d1279624738e174290ce3c7e21c0504
konbra99 <250339@student.pwr.edu.pl>
2022-05-24 12:26:56 +0200

    dane

--- runTests.py
+++ runTests.py
@@ -1,6 +1,9 @@
+import sys
 from algorytm import algorithm
+from etap2perm import solverAlgorithm
 import numpy as np
 from timeit import default_timer as timer
+import localsolver
 import os
 
 
@@ -10,40 +13,71 @@ class Statistic:
         self.minValue = results.min()
         self.maxValue = results.max()
         self.meanValue = np.mean(results)
-    
+
     def __str__(self):
         return f'{self.minValue};{self.meanValue};{self.maxValue}'
 
 
-def measuredAlgorithm(file):
-    before = timer()
-    makespan = algorithm(file)
-    after = timer()
-    return makespan, after - before
+def measureTime(funciton):
+    def measured(*args, **kwargs):
+        before = timer()
+        result = funciton(*args, **kwargs)
+        after = timer()
+        return result, after - before
+
+    return measured
 
 
-def runTests(testsFolder):
+def runTests(testsFolder, algo='algo'):
+    testedAlgorithm = algorithm if algo == 'algo' else solverAlgorithm
+    testedAlgorithm = measureTime(testedAlgorithm)
     for _, _, files in os.walk(testsFolder):
         makespans = []
         times = []
         for file in files:
-            makespan, time = measuredAlgorithm(f'{testsFolder}/{file}')
+
+            makespan, time = testedAlgorithm(f'{testsFolder}/{file}')
             makespans.append(makespan)
             times.append(time)
-        
-        print(f'{testsFolder};{Statistic(makespans)};{Statistic(times)}')
+
+        type = ';'.join(testsFolder.split('\\')[-1].split('_'))
+        return f'{type};{Statistic(makespans)};{Statistic(times)}'
+
+
+def runGroupedTests(algo='algo'):
+    short = []
+    for root, dirs, _ in os.walk('short_tests'):
+        for dir in dirs:
+            short.append(runTests(os.path.join(root, dir), algo))
+
+    long = []
+    for root, dirs, _ in os.walk('long_tests'):
+        for dir in dirs:
+            long.append(runTests(os.path.join(root, dir), algo))
+    
+    return short, long
+
+
+def main(algo, outFile):
+    algo = sys.argv[1]
+    jobs = [5, 10, 20, 30, 40, 50]
+    machines = [5, 10, 20, 30, 40, 50]
+    results = []
+    #for j in jobs:
+    #    results.append(runTests(f'jobs_{j}', algo))
+    #for m in machines:
+    #    results.append(runTests(f'machines_{m}', algo))
+    s, l = runGroupedTests(algo)
+    with open(outFile+'_short.csv', 'w') as f:
+        f.write('n;m;min_makespan;avg_makespan;max_makespan;min_time;avg_time;max_time\n')
+        for result in s:
+            f.write(result + '\n')
+    with open(outFile+'_long.csv', 'w') as f:
+        f.write('n;m;min_makespan;avg_makespan;max_makespan;min_time;avg_time;max_time\n')
+        for result in l:
+            f.write(result + '\n')
 
 
 if __name__ == '__main__':
-    runTests('C:\dev\osis\jobs_5')
-    runTests('C:\dev\osis\jobs_10')
-    runTests('C:\dev\osis\jobs_20')
-    runTests('C:\dev\osis\jobs_30')
-    runTests('C:\dev\osis\jobs_40')
-    runTests('C:\dev\osis\jobs_50')
-    runTests('C:\dev\osis\machines_5')
-    runTests('C:\dev\osis\machines_10')
-    runTests('C:\dev\osis\machines_20')
-    runTests('C:\dev\osis\machines_30')
-    runTests('C:\dev\osis\machines_40')
-    runTests('C:\dev\osis\machines_50')
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
